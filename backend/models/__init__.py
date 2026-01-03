70d075adfe82c84c2da83dda8ac44d0c9f9cef8e
Bryan Lee <bryan.lee@theaccessgroup.com>
2026-01-03 16:50:59 +0000

    refactor: Update model imports in backend

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -6,10 +6,10 @@ This package contains all SQLAlchemy database models.
 Import all models here so Alembic can detect them for migrations.
 """
 
-from backend.models.user import User
-from backend.models.sensor_reading import SensorReading
-from backend.models.pest_detection import PestDetection, SeverityLevel
-from backend.models.alert import Alert, AlertType, AlertSeverity
+from .user import User
+from .sensor_reading import SensorReading
+from .pest_detection import PestDetection, SeverityLevel
+from .alert import Alert, AlertType, AlertSeverity
 
 # Export all models
 __all__ = [
