ea454ec60c4a6fd853f86c289a496977f57beba2
Bryan Lee <bryan.lee@theaccessgroup.com>
2026-01-03 17:17:16 +0000

    refactor: Update backend utils import statement

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -4,7 +4,7 @@ Utilities Package
 This package contains utility functions and helpers.
 """
 
-from backend.utils.password import verify_password, get_password_hash
+from .password import verify_password, get_password_hash
 
 __all__ = [
     "verify_password",
